4f201a9c1df5d87b10a4e309148c88b537ef08dd
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2024-09-15 15:47:07 +0200

    working flashing and download

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use anyhow;
+use anyhow::{self, Context};
 use clap::{self, Parser};
 use etcetera::{self, AppStrategy, AppStrategyArgs};
-use probe_rs::probe::{list::Lister, Probe};
+use probe_rs::flashing::{
+    download_file_with_options, DownloadOptions, FlashProgress, Format, ProgressEvent,
+};
+use probe_rs::probe::list::Lister;
 use probe_rs::Permissions;
 use std::fs;
-use std::io::{self, BufRead};
-use std::path::{self, Path};
 use std::process::exit;
 
 mod config;
@@ -17,22 +18,17 @@ mod utils;
 #[command(version, about, long_about = None)]
 struct Args {
     firmware_name: Option<String>,
+    firmware_version: Option<String>,
+
+    /// Lists available firmware names (if given no arguments) or versions of a specific firmware (if given FIRMWARE_NAME)
+    #[arg(long, short)]
+    list: bool,
 
     #[arg(long)]
-    list: bool,
+    clear_cache: bool,
 }
 
 fn main() -> anyhow::Result<()> {
-    /*let s3_path = "test.file";
-    let test = b"I'm going to S3!"; */
-
-    //let response_data = bucket.put_object(s3_path, test).await?;
-    //assert_eq!(response_data.status_code(), 200);
-
-    //let response_data = bucket.get_object(s3_path).await?;
-    //assert_eq!(response_data.status_code(), 200);
-    //assert_eq!(test, response_data.as_slice());
-
     let args = Args::parse();
 
     let strategy = etcetera::choose_app_strategy(AppStrategyArgs {
@@ -40,60 +36,125 @@ fn main() -> anyhow::Result<()> {
         author: "manakjiri".to_string(),
         app_name: "quick-flash".to_string(),
     })?;
-    fs::create_dir_all(strategy.config_dir())?;
+    fs::create_dir_all(strategy.config_dir()).context("Failed to create config directory")?;
     let creds_path = strategy.config_dir().join("credentials.toml");
+    let cache_base = strategy.cache_dir().join("firmware");
+    fs::create_dir_all(&cache_base).context("Failed to create cache directory")?;
 
-    let creds = config::get_credentials(&creds_path)?;
-    let storage = storage::Storage::new(&creds)?;
+    if args.clear_cache {
+        eprintln!("Clearing cache directory...");
+        fs::remove_dir_all(&cache_base).context("Failed to clear cache directory")?;
+        fs::create_dir_all(&cache_base)?;
+    }
 
-    if args.list {
-        match args.firmware_name {
-            Some(name) => {
-                for version in storage.list_firmware_versions(&name)? {
-                    println!("{}", version);
-                }
-            }
-            None => {
-                for name in storage.list_firmwares()? {
-                    println!("{}", name);
-                }
-            }
+    let creds = config::get_credentials(&creds_path).context("Failed to read credentials")?;
+    let storage = storage::Storage::new(&creds).context("Failed to create storage client")?;
+
+    let firmwares = storage
+        .list_firmwares()
+        .context("Failed to fetch firmware names from the Bucket")?;
+
+    if firmwares.is_empty() {
+        anyhow::bail!("No firmware found in the Bucket");
+    }
+
+    /* firmware names list command */
+    if args.list && args.firmware_name.is_none() {
+        println!(
+            "Listing {} available firmware name{}:",
+            firmwares.len(),
+            firmwares.len().eq(&1).then_some("").unwrap_or("s")
+        );
+        for name in storage.list_firmwares()? {
+            println!("  - {}", name);
         }
         exit(0);
     }
 
-    /* let response_data = bucket.list("".to_string(), None)?;
-    println!(
-        "{:?}",
-        response_data[0]
-            .contents
-            .iter()
-            .map(|f| f.key.to_owned())
-            .collect::<Vec<String>>()
-    ); */
+    /* firmware name sanity checks */
+    let firmware_name = match args.firmware_name {
+        Some(n) => {
+            if !firmwares.contains(&n) {
+                anyhow::bail!("Firmware name \"{}\" not found in the Bucket", n)
+            }
+            n
+        }
+        None => {
+            anyhow::bail!(
+                "Please specify firmware name to continue (you can use the --list option to list all names)"
+            )
+        }
+    };
 
-    //let response_data = bucket.delete_object(s3_path).await?;
-    //assert_eq!(response_data.status_code(), 204);
-    Ok(())
-}
+    let versions = storage
+        .list_firmware_versions(&firmware_name)
+        .context("Failed to fetch firmware versions from the Bucket")?;
 
-/* fn main() -> Result<(), probe_rs::Error> {
+    /* firmware version list command */
+    if args.list && args.firmware_version.is_none() {
+        println!(
+            "Listing {} version{} of firmware \"{}\"",
+            versions.len(),
+            versions.len().eq(&1).then_some("").unwrap_or("s"),
+            firmware_name
+        );
+        for version in versions {
+            println!("  - {}", version);
+        }
+        exit(0);
+    } else if args.list && args.firmware_version.is_some() {
+        anyhow::bail!("Invalid use of the --list command")
+    }
+
+    /* firmware version sanity checks */
+    let firmware_version = match args.firmware_version {
+        Some(v) => {
+            if !versions.contains(&v) {
+                anyhow::bail!("Firmware version \"{}\" not found in the Bucket", v)
+            }
+            v
+        }
+        None => {
+            anyhow::bail!(
+                "Please specify firmware version to continue (you can use the --list option to list all versions)"
+            )
+        }
+    };
+
+    /* Finally onto the firmware flashing itself */
     // Get a list of all available debug probes.
     let lister = Lister::new();
-
     let probes = lister.list_all();
-
     // Use the first probe found.
-    let mut probe = probes[0].open()?;
+    let probe = probes[0].open().context("Failed to open probe")?;
+
+    let firmware = storage
+        .download_firmware(&firmware_name, &firmware_version, &cache_base)
+        .context("Failed to download firmware")?;
 
     // Attach to a chip.
-    let mut session = probe.attach("STM32F103RB", Permissions::default())?;
+    let mut session = probe
+        .attach_under_reset(&firmware.chip, Permissions::default())
+        .context("Failed to attach probe")?;
+    // Download the firmware binary.
 
-    // Select a core.
-    let mut core = session.core(0)?;
+    eprintln!(
+        "Downloading {}/{} to target chip {}...",
+        firmware.name, firmware.version, firmware.chip
+    );
+    let mut options = DownloadOptions::default();
+    options.progress = Some(FlashProgress::new(|e| match e {
+        ProgressEvent::StartedErasing => eprintln!("Flash erasing..."),
+        ProgressEvent::FinishedErasing => eprintln!("Flash programming..."),
+        _ => {}
+    }));
+    options.verify = true;
+    options.do_chip_erase = true;
+    download_file_with_options(&mut session, firmware.path, Format::Elf, options)
+        .context("Failed to flash firmware")?;
 
-    // Halt the attached core.
-    core.halt(std::time::Duration::from_millis(10))?;
+    eprintln!("Resetting target...");
+    session.core(0)?.reset()?;
 
     Ok(())
-} */
+}
--- src/storage.rs
+++ src/storage.rs
@@ -1,6 +1,21 @@
 use crate::config::{Credentials, StorageType};
 use anyhow::{self, Context};
 use s3;
+use serde::{Deserialize, Serialize};
+use std::path::{Path, PathBuf};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Firmware {
+    pub name: String,
+    pub version: String,
+    pub chip: String,
+    pub path: PathBuf,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Manifest {
+    chip: String,
+}
 
 pub struct Storage {
     bucket: Box<s3::Bucket>,
@@ -54,5 +69,38 @@ impl Storage {
             .iter()
             .map(|p| p.strip_prefix(&firmware_name).unwrap_or(&p).to_owned())
             .collect())
+    }
+
+    pub fn download_firmware(
+        &self,
+        name: &str,
+        version: &str,
+        cache_base: &Path,
+    ) -> anyhow::Result<Firmware> {
+        let cache_base = cache_base.to_path_buf().join(name).join(version);
+        let cache_firmware = cache_base.join("firmware.elf");
+        let cache_manifest = cache_base.join("manifest.json");
+
+        if !cache_base.exists() {
+            std::fs::create_dir_all(&cache_base)?;
+            let bucket_base = format!("{}/{}", name, version);
+            let bucket_firmware = format!("{}/firmware.elf", bucket_base);
+            let bucket_manifest = format!("{}/manifest.json", bucket_base);
+
+            eprintln!("Downloading firmware to {}...", cache_base.display());
+            let firmware = self.bucket.get_object(&bucket_firmware)?;
+            std::fs::write(&cache_firmware, firmware.bytes())?;
+            let manifest = self.bucket.get_object(&bucket_manifest)?;
+            std::fs::write(&cache_manifest, manifest.bytes())?;
+        }
+
+        let manifest: Manifest = serde_json::from_str(&std::fs::read_to_string(cache_manifest)?)?;
+
+        Ok(Firmware {
+            name: name.to_owned(),
+            version: version.to_owned(),
+            chip: manifest.chip,
+            path: cache_firmware,
+        })
     }
 }
